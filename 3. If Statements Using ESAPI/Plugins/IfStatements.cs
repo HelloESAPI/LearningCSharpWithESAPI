8568d75c81e894c47b6c6420d4bdf0dba1b1b807
hello-esapi <thecodingdosi@gmail.com>
2021-10-26 05:37:51 -0400

    covered the basics of using If statements

--- 3. If Statements Using ESAPI/Plugins/IfStatements.cs
+++ 3. If Statements Using ESAPI/Plugins/IfStatements.cs
@@ -21,7 +21,63 @@ namespace VMS.TPS
     [MethodImpl(MethodImplOptions.NoInlining)]
     public void Execute(ScriptContext context /*, System.Windows.Window window, ScriptEnvironment environment*/)
     {
-      // TODO : Add here the code that is called when the script is launched from Eclipse.
+      // truth test
+      // if statement
+      // if patient not open in eclipse
+      if (context.Patient == null) // will enter logic if true
+      {
+        MessageBox.Show("Please open a patient");
+        return;
+      }
+      // if a structure set not open in eclipse
+      if (context.StructureSet == null) // will enter logic if true
+      {
+        MessageBox.Show("Please open a structureset");
+        return;
+      }
+
+      // list of gtvs
+      List<Structure> gtvs = new List<Structure>();
+      // list of ctvs
+      List<Structure> ctvs = new List<Structure>();
+      // list of ptvs
+      List<Structure> ptvs = new List<Structure>();
+      // list of all other structures
+      List<Structure> oars = new List<Structure>();
+
+      // filter structures using if statment
+      foreach (var s in context.StructureSet.Structures)
+      {
+        if (s.Id.ToUpper().Contains("GTV"))
+        {
+          gtvs.Add(s);
+        }
+        else if (s.Id.ToUpper().Contains("CTV"))
+        {
+          ctvs.Add(s);
+        }
+        else if (s.Id.ToUpper().Contains("PTV"))
+        {
+          ptvs.Add(s);
+        }
+        else
+        {
+          oars.Add(s);
+        }
+      }
+
+
+      // clear lists
+      gtvs.Clear();
+      ctvs.Clear();
+      ptvs.Clear();
+      oars.Clear();
+
+      
+
+
+
+
     }
   }
 }
