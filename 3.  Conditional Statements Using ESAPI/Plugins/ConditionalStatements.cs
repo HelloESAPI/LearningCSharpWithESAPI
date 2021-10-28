591f264d2034dfac59321a0df28f25b3fb9e6d63
hello-esapi <thecodingdosi@gmail.com>
2021-10-28 07:17:41 -0400

    simple intro to conditional statements -> if, else if, else, switch, and single line conditional operators

--- 3.  Conditional Statements Using ESAPI/Plugins/ConditionalStatements.cs
+++ 3.  Conditional Statements Using ESAPI/Plugins/ConditionalStatements.cs
@@ -18,14 +18,21 @@ namespace VMS.TPS
     {
     }
 
+
+    // structure lists -- used for filtering
+    private List<Structure> gtvs = new List<Structure>();
+    private List<Structure> ctvs = new List<Structure>();
+    private List<Structure> ptvs = new List<Structure>();
+    private List<Structure> oars = new List<Structure>();
+    private List<Structure> structures = new List<Structure>();
+
     [MethodImpl(MethodImplOptions.NoInlining)]
     public void Execute(ScriptContext context /*, System.Windows.Window window, ScriptEnvironment environment*/)
     {
-      // Truth Statements
+      // Conditional Statements -> Truth Statements
       // - If, else if, else
-      // - switch case
-      // - one liners
-
+      // - switch
+      // - one liners -- aka conditional operators
 
       // if statements
       if (context.Patient == null || context.StructureSet == null || context.PlanSetup == null)
@@ -34,11 +41,10 @@ namespace VMS.TPS
         return;
       }
 
-      // filter structures into different lists
-      List<Structure> gtvs = new List<Structure>();
-      List<Structure> ctvs = new List<Structure>();
-      List<Structure> ptvs = new List<Structure>();
-      List<Structure> oars = new List<Structure>();
+
+      // OR => ||
+      // AND => &&
+
 
 
       //foreach (var s in context.StructureSet.Structures)
@@ -60,26 +66,32 @@ namespace VMS.TPS
         }
         else
         {
-          oars.Add(s);
+          if (s.Id.ToUpper().Contains("COUCH") == false) 
+          { 
+            oars.Add(s); 
+            structures.Add(s); 
+          }
         }
         //}
       }
 
       // clear the lists of structures
-      gtvs.Clear();
-      ctvs.Clear();
-      ptvs.Clear();
-      oars.Clear();
+      //gtvs.Clear();
+      //ctvs.Clear();
+      //ptvs.Clear();
+      //oars.Clear();
 
-      // switch case statement - or case switch
-
+      // switch statement - or case switch
       // loop through structures that have a segment and are not empty
       foreach (var s in context.StructureSet.Structures.Where(x => x.IsEmpty == false && x.HasSegment == true))
       {
         switch (s.DicomType)
         {
           case "GTV":
-            gtvs.Add(s);
+            if (gtvs.Contains(s) == false) // example of ignoring duplicates
+            {
+              gtvs.Add(s);
+            }
             break;
           case "CTV":
             ctvs.Add(s);
@@ -87,33 +99,49 @@ namespace VMS.TPS
           case "PTV":
             ptvs.Add(s);
             break;
+          case "ORGAN":
+            oars.Add(s);
+            break;
 
           default:
-            oars.Add(s);
+            //structures.Add(s);
             break;
         }
       }
 
+      // example of filtering out duplicates
+      //foreach (var gtv in gtvs.Distinct())
+      //{
 
-      // conditional operators
+      //}
+
+
+      // conditional operators -> ?:
       foreach (var s in context.StructureSet.Structures)
       {
 
         // long way 
-        string isEmpty = string.Empty;
-        if (s.IsEmpty == true)
-        {
-          isEmpty = "Yes";
-        }
-        else
-        {
-          isEmpty = "No";
-        }
+        //string isEmpty = string.Empty;
+        //if (s.IsEmpty == true)
+        //{
+        //  isEmpty = "Yes";
+        //}
+        //else
+        //{
+        //  isEmpty = "No";
+        //}
 
         // one line truth test
-        isEmpty = s.IsEmpty == true ? "Yes" : "No";
+        string isEmpty = (s.IsEmpty == true) ? "Yes" : "No";
         string isPTV = s.DicomType == "PTV" ? "Yes" : "No";
+        string targetType = s.DicomType == "PTV" ? "PTV" : s.DicomType == "CTV" ? "CTV" : "Other";
         bool isPTVBoolean = s.DicomType == "PTV" ? true : false;
+
+        if (isPTVBoolean == true)
+        {
+          // do stuff...
+        }
+
         bool isPTVBooleanSimplified = s.DicomType == "PTV";
       }
     }
