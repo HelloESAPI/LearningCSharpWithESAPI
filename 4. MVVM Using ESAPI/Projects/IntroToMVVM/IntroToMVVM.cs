31e896fa95b3c2876fb3802e0f7ae618ae9661c6
hello-esapi <thecodingdosi@gmail.com>
2021-10-30 05:43:43 -0400

    setup basic mvvm pattern and saved template

--- 4. MVVM Using ESAPI/Projects/IntroToMVVM/IntroToMVVM.cs
+++ 4. MVVM Using ESAPI/Projects/IntroToMVVM/IntroToMVVM.cs
@@ -7,6 +7,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using VMS.TPS.Common.Model.API;
 using VMS.TPS.Common.Model.Types;
+using IntroToMVVM.Views;
+using IntroToMVVM.ViewModels;
 
 // TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
 [assembly: AssemblyVersion("1.0.0.1")]
@@ -18,6 +20,7 @@ using VMS.TPS.Common.Model.Types;
 
 namespace VMS.TPS
 {
+
   public class Script
   {
     public Script()
@@ -25,9 +28,19 @@ namespace VMS.TPS
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    public void Execute(ScriptContext context /*, System.Windows.Window window, ScriptEnvironment environment*/)
+    public void Execute(ScriptContext context, System.Windows.Window window/*, ScriptEnvironment environment*/)
     {
       // TODO : Add here the code that is called when the script is launched from Eclipse.
+      // MVVM
+      // - Model
+      // - View
+      // - Viewmodel
+
+      // new instance of our main view - user control
+      MainView mainView = new MainView { DataContext = new MainViewModel() };
+
+      // set the eclipse script's window content to show our view
+      window.Content = mainView;
     }
   }
 }
