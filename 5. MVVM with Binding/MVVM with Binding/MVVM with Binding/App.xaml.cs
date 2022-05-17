21598aabecfd19b14373b8636a855fc3c90e32f5
hello-esapi <thecodingdosi@gmail.com>
2022-05-17 05:51:13 -0400

    adding a project example for mvvm and binding

---
+++ 5. MVVM with Binding/MVVM with Binding/MVVM with Binding/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using MVVM_with_Binding.ViewModels;
+using MVVM_with_Binding.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM_with_Binding
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+      MainView mainView = new MainView { DataContext = new MainViewModel() };
+      mainView.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+      mainView.ShowDialog();
+    }
+  }
+}
